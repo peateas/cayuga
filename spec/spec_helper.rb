de9318d5a90195c4b643fa97d49b6dd3f00cd3c0
patrick <peateas@gmail.com>
2019-03-14 11:52:44 +0100

    refactoring:  removal of web interface, cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ require 'factory_information_helper'
 require 'logger_information_helper'
 require 'tool_information_helper'
 require 'alternate_factory_helper'
-require 'browser_helper'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -30,9 +29,8 @@ RSpec.configure do |config|
   # noinspection RubyResolve
   config.include(AlternateFactoryHelper, :alternate_factory)
   # noinspection RubyResolve
-  config.include(BrowserHelper, :browser)
 
-  config.filter_run_excluding :slow
+  # config.filter_run_excluding :slow
 
 end
 
